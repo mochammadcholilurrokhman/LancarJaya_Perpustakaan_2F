a2c2494a2a4a914da353b0e7d8eac2a0da16f3c3
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2023-11-30 12:18:01 +0700

    perpustakaan

---
+++ komponen/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../style/header.css">
+</head>
+
+<body>
+    <header>
+        <h1>Ruang Baca</h1>
+        <div class="profile-login">
+            <a href="login.php" class="login-link">Login</a>
+
+            <?php
+            $currentPage = basename($_SERVER['PHP_SELF']);
+
+            $imageUrl = ($currentPage === 'landingpage.php') ? 'img/user.png' : '../img/user.png';
+            ?>
+
+            <a href="#"><img class="user" src="<?php echo $imageUrl; ?>" alt="User"></a>
+        </div>
+
+        <div class="header"></div>
+    </header>
+</body>
+
+</html>
--- landingpage.php
+++ landingpage.php
@@ -2,99 +2,34 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lancar Jaya</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/landingpage.css">
+  <link rel="stylesheet" href="style/header.css">
 
-        header {
-            background-color: rgba(0, 173, 181, 1);
-            padding: 2px;
-            color: white;
-            text-align: start;
-            position: relative;
-        }
-
-        .profile-login {
-            position: absolute;
-            top: 50%;
-            right: 20px;
-            transform: translateY(-50%);
-            font-size: 14px;
-        }
-
-        .logo-container {
-            text-align: center;
-            margin-top: 20px;
-            font-family: roboto;
-        }
-
-        .logo {
-            width: 80px;
-            height: auto;
-        }
-
-        .website-name {
-            margin-top: 10px;
-            font-size: 20px;
-            font-family: roboto;
-            color: rgba(0, 173, 181, 1);
-        }
-
-        .website-name1 {
-            color: rgba(251, 146, 60, 1);
-        }
-
-        .features {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        .feature {
-            display: inline-block;
-            margin: 0 20px;
-            font-size: 10px;
-            background-color: rgba(0, 173, 181, 1);
-            color: white;
-            padding: 10px;
-           
-        }
-
-        .user {
-            width: 20px;
-            height: 20px;
-            border-radius: 50%;
-            margin-right: 5px;
-        }
-    </style>
+  <title>Ruang Baca</title>
+  
 </head>
 
 <body>
 
-    <header>
-        <h1>Ruang Baca</h1>
-        <div class="profile-login">
-            <a href="login.php">Login</a>
-            <a><img class="user" src="asessts/user.png"></a>
-        </div>
-    </header>
+<?php include('komponen/header.php'); ?>
 
-    <div class="logo-container">
-        <img class="logo" src="asessts/logo.png" alt="Logo Website">
-        <div class="website-name">Dashboard</div>
-        <div class="website-name1">Ruang Baca</div>
-    </div>
+  <div class="logo-container">
+    <img class="logo" src="img/logo.png" alt="Logo Website">
+  </div>
+  <div class="website-names">
+    <div class="website-name">Dashboard</div>
+    <div class="website-name1">Ruang Baca</div>
+  </div>
 
-    <div class="features">
-        <div class="feature"><a href="fitur/katalog.php">Katalog</a></div>
-        <div class="feature"> <a href="fitur/repository.php">Repository</a></div>
-        <div class="feature"><a href="fitur/peraturan.php">Peraturan</a></div>
-    </div>
+  <div class="features">
+    <div class="feature-container"><a href="fitur/katalog.php">Katalog</a></div>
+    <div class="feature-container"><a href="fitur/repository.php">Repository</a></div>
+    <div class="feature-container"><a href="fitur/tentangkami.php">Tentang Kami</a></div>
+    <div class="feature-container"><a href="fitur/peraturan.php">Peraturan</a></div>
+  </div>
+
 </body>
 
 </html>
