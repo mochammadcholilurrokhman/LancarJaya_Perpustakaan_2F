566c3b403d6503f5949d5c53db2db7d4f499727d
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2023-12-04 07:18:34 +0700

    perpustakaan

--- komponen/sidebar.php
+++ komponen/sidebar.php
@@ -41,16 +41,6 @@
         <a href="../fitur/repository.php">Repository </a>
       </div>
 
-      <!-- aboutUs Sidebar -->
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all <?php echo ($currentPage === 'aboutUs') ? 'bg-gray-300' : 'hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80'; ?> hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-5 w-5">
-            <path fill-rule="evenodd" d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z" clip-rule="evenodd"></path>
-          </svg>
-        </div>
-        <a href="../fitur/aboutUs.php">About Us</a>
-      </div>
-
       <!-- Rules Sidebar -->
       <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all <?php echo ($currentPage === 'rules') ? 'bg-gray-300' : 'hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80'; ?> hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
         <div class="grid place-items-center mr-4">
