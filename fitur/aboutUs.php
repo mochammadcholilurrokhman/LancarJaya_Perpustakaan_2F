ad13b51c8852062c8a67a9162459342c3ffa3302
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2023-12-04 07:07:57 +0700

    perpustakaan

--- fitur/aboutUs.php
+++ fitur/aboutUs.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Katalog Perpustakaan</title>
-    <link rel="stylesheet" href="../style/aboutUs.css">
+    <link rel="stylesheet" href="style/aboutUs.css">
 </head>
 
 <body>
@@ -18,9 +18,9 @@
                 Dilengkapi dengan tools yang memadai untuk kenyamanan pengunjung <br>
                 seperti meja diskusi, AC, tangga buku, dan masih banyak lagi. <br> <br>
             </p>
-            <p> Jam operasional Ruang Baca JTI : 
-                <br> Hari : Senin - Jumat 
-                <br> Pukul : 07.00 - 16.00 
+            <p> Jam operasional Ruang Baca JTI :
+                <br> Hari : Senin - Jumat
+                <br> Pukul : 07.00 - 16.00
                 <br> <br>
             </p>
             <p> Dengan koleksi literatur yang cukup kompleks beserta fasilitas modern, <br>
@@ -29,19 +29,19 @@
                 tetapi juga menjadi pusat inspirasi bagi civitas akademika <br>
                 Jurusan Teknologi Informasi Politeknik Negeri Malang dalam berdiskusi.</p>
             </p> <br><br>
-                
+
             <div class="info-bar">
-                <span> 
+                <span>
                     <b>Alamat: </b>
-                    <br> Gedung JTI Lt 6. 
-                    <br> Jl.Soekarno Hatta No. 9 Malang 
-                    <br> Telp : (0xx1) 4xx24, 4xxx25 
+                    <br> Gedung JTI Lt 6.
+                    <br> Jl.Soekarno Hatta No. 9 Malang
+                    <br> Telp : (0xx1) 4xx24, 4xxx25
                     <br> Fax: (0XX1) 4XXX20
                     <br> <br>
                 </span>
-                    <span> <b>Social Media: </b> 
+                <span> <b>Social Media: </b>
                     <br> instagram
-                    <br> facebook 
+                    <br> facebook
                     <br> twitter
                 </span>
             </div>
