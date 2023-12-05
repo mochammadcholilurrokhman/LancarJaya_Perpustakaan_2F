574bee02a15080d32690a1e515baffb0c7f91844
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2023-12-05 11:34:14 +0700

    perpustakaan

--- cek_login.php
+++ cek_login.php
@@ -1,4 +1,3 @@
-
 <?php 
 // mengaktifkan session php
 session_start();
@@ -11,17 +10,27 @@ $username = $_POST['username'];
 $psw = $_POST['password'];
  
 // menyeleksi data admin dengan username dan password yang sesuai
-$data = mysqli_query($connection,"SELECT * from user where username='$username' and password='$psw'");
+$data = mysqli_query($connection, "SELECT * FROM user WHERE username='$username' AND password='$psw'");
  
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($data);
  
 if($cek > 0){
-	$_SESSION['username'] = $username;
-    $_SESSION['level'] = "user";
-	$_SESSION['status'] = "login";
-	header("location:user/dashboard.php");
-}else{
-	header("location:login.php?pesan=gagal");
+    $row = mysqli_fetch_assoc($data);
+    if ($row['posisi'] == "admin") {
+        // Admin login
+        $_SESSION['username'] = $username;
+        $_SESSION['posisi'] = "admin";
+        $_SESSION['status'] = "login";
+        header("location:admin/dashboardadmin.php");
+    } else {
+        // Regular user login
+        $_SESSION['username'] = $username;
+        $_SESSION['posisi'] = "user";
+        $_SESSION['status'] = "login";
+        header("location:user/dashboard.php");
+    }
+} else {
+    header("location:login.php?pesan=gagal");
 }
 ?>
