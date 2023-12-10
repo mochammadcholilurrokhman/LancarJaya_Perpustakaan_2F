f4702a8ec87a1967763f0e9ade8eb523fad80c46
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2023-12-10 14:35:24 +0700

    search fitur katalog admin

--- landingpage.php
+++ landingpage.php
@@ -7,6 +7,33 @@
   <link rel="stylesheet" href="style/landingpage.css">
   <link rel="stylesheet" href="style/header.css">
 
+  <style>
+  .hero {
+  min-height: 80vh;
+  align-items: center;
+  background-image: url("img/page.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
+.features {
+  background: none;
+}
+.hero::before {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 30%;
+  bottom: 0;
+  background: linear-gradient(
+    0deg,
+    rgba(1, 1, 3, 1) 8%,
+    rgba(255, 255, 255, 0) 10%
+  );
+}
+  </style>
   <title>Ruang Baca</title>
 
 </head>
@@ -14,7 +41,7 @@
 <body>
 
   <?php include("komponen/header.php"); ?>
-
+<section class="hero">
   <div class="logo-container">
     <img class="logo" src="img/logo.png" alt="Logo Website">
   </div>
@@ -27,6 +54,7 @@
     <div class="feature-container"><a href="fitur/repository.php">Repository</a></div>
     <div class="feature-container"><a href="fitur/peraturan.php">Peraturan</a></div>
   </div>
+  </section>
   <?php include('fitur/aboutUs.php'); ?>
 
 </body>
